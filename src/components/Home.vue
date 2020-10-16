b79da746f9c1ba03695703c2f0f3f5e4bbd1cfb4
陈思敏(大族智控) <chensm700318@hanslaser.com>
2020-10-16 09:53:05 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<!-- home组件 -->
+<template>
+    <div class=''>
+        <el-button type='info' @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
